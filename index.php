fd13f0adb1acc6c49bde6bd242bf04f2f2c1986a
Guigozor <xavier.gui@hotmail.com>
2017-10-10 19:38:14 -0300

    novo commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>SAS Web</title>
+        <title>SAS Web - Seja bem vindo :)</title>
     </head>
     <body>
         <?php
